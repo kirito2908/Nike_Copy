5504b239c0bd470d6fe09c996b4f94ae61cfdd9d
user <user@users-MacBook-Pro.local>
2024-05-25 16:51:29 +0530

    routing

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from './Header'
+import BodyPart from './BodyPart'
+
+const Home = () => {
+  return (
+    <>
+      <Header/>
+      <BodyPart/>
+    </>
+  )
+}
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Header } from "./Components/Header";
-import BodyPart from "./Components/BodyPart";
-import Products from "./Components/Products";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import ForMen from "./Components/ForMen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <Header />
-    <BodyPart />
-    <Products />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/catagory/men" element={<ForMen/>}/>
+        {/* <Route path="/catagory/women" element={<ForWomen/>}/>
+        <Route path="/catagory/kids" element={<ForKids/>}/> */}
+      </Routes>
+    </BrowserRouter>
   </>
 );
